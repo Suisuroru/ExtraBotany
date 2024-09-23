221fe46a41e17372973408cf778d8981f7eb5e0b
grasspow <grasspow@outlook.com>
2024-09-23 10:35:04 +0800

    [refactor]: refactor the model generation of normal blockitem

--- src/main/java/io/grasspow/extrabotany/data/BlockStateProvider.java
+++ src/main/java/io/grasspow/extrabotany/data/BlockStateProvider.java
@@ -3,7 +3,9 @@ package io.grasspow.extrabotany.data;
 import io.grasspow.extrabotany.common.block.ExtraBotanyBlocks;
 import io.grasspow.extrabotany.common.libs.LibMisc;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 
 public class BlockStateProvider extends net.minecraftforge.client.model.generators.BlockStateProvider {
     public BlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
@@ -12,7 +14,12 @@ public class BlockStateProvider extends net.minecraftforge.client.model.generato
 
     @Override
     protected void registerStatesAndModels() {
-        simpleBlock(ExtraBotanyBlocks.PHOTONIUM_BLOCK.get());
-        simpleBlock(ExtraBotanyBlocks.SHADOWIUM_BLOCK.get());
+        normal(ExtraBotanyBlocks.PHOTONIUM_BLOCK);
+        normal(ExtraBotanyBlocks.SHADOWIUM_BLOCK);
+        simpleBlock(ExtraBotanyBlocks.PEDESTAL.get());
+    }
+
+    private void normal(RegistryObject<Block> block) {
+        simpleBlockWithItem(block.get(), cubeAll(block.get()));
     }
 }
