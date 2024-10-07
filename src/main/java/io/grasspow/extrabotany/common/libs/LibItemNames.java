3be17a71eb5f9c3481468d4d1593317270d91233
grasspow <grasspow@outlook.com>
2024-10-07 20:46:07 +0800

    [chore]: rename, edit props. (do these just make me feel relaxed.)

--- src/main/java/io/grasspow/extrabotany/common/libs/LibItemNames.java
+++ src/main/java/io/grasspow/extrabotany/common/libs/LibItemNames.java
@@ -3,10 +3,10 @@ package io.grasspow.extrabotany.common.libs;
 public class LibItemNames {
 
     public static final String MOTOR = "motor";
-    public static final String GEMOFCONQUEST = "gemofconquest";
-    public static final String COSMICCARKEY = "cosmiccarkey";
-    public static final String FIRSTFRACTAL = "firstfractal";
-    public static final String FLAMESCIONWEAPON = "flamescionweapon";
+    public static final String GEM_OF_CONQUEST = "gem_of_conquest";
+    public static final String COSMIC_CAR_KEY = "cosmic_car_key";
+    public static final String FIRST_FRACTAL = "first_fractal";
+    public static final String FLAME_SCION_WEAPON = "flame_scion_weapon";
 
     public static final String WALKINGCANE = "walkingcane";
     public static final String MANA_READER = "mana_reader";
@@ -16,7 +16,7 @@ public class LibItemNames {
     public static final String DEATH_RING = "death_ring";
     public static final String NATURE_ORB = "nature_orb";
     public static final String JINGWEI_FEATHER = "jingwei_feather";
-    public static final String SILVERBULLET = "silverbullet";
+    public static final String SILVER_BULLET = "silverbullet";
 
     public static final String PEACE_AMULET = "peace_amulet";
     public static final String AERO_STONE = "aero_stone";
@@ -25,35 +25,35 @@ public class LibItemNames {
     public static final String IGNIS_STONE = "ignis_stone";
     public static final String THE_COMMUNITY = "the_community";
     public static final String POWER_GLOVE = "power_glove";
-    public static final String MOTORACCESSORY = "motoraccessory";
-    public static final String COSMICCARKEYACCESSORY = "cosmiccarkeyaccessory";
-    public static final String SUNRING = "sunring";
-    public static final String MOONPENDANT = "moonpendant";
+    public static final String MOTOR_ACCESSORY = "motoraccessory";
+    public static final String COSMIC_CAR_KEY_ACCESSORY = "cosmiccarkeyaccessory";
+    public static final String SUN_RING = "sun_ring";
+    public static final String MOON_PENDANT = "moon_pendant";
     public static final String POTATO_CHIPS = "potato_chips";
 
     public static final String RECORD_EGO = "record_ego";
     public static final String RECORD_HERRSCHER = "record_herrscher";
 
-    public static final String SHADOWKATANA = "shadowkatana";
-    public static final String RODOFDISCORD = "rodofdiscord";
+    public static final String SHADOW_KATANA = "shadow_katana";
+    public static final String ROD_OF_DISCORD = "rod_of_discord";
 
     public static final String JUDAHOATH = "judahoath";
-    public static final String JUDAHOATHSAKURA = "judahoathsakura";
-    public static final String JUDAHOATHIDOL = "judahoathidol";
+    public static final String JUDAHOATH_SAKURA = "judahoath_sakura";
+    public static final String JUDAHOATH_IDOL = "judahoath_idol";
 
-    public static final String UUZFAN = "uuzfan";
-    public static final String TRUETERRABLADE = "trueterrablade";
-    public static final String TRUESHADOWKATANA = "trueshadowkatana";
-    public static final String INFLUXWAVER = "influxwaver";
-    public static final String STARWRATH = "starwrath";
+    public static final String UUZ_FAN = "uuz_fan";
+    public static final String TRUE_TERRABLADE = "true_terrablade";
+    public static final String TRUE_SHADOW_KATANA = "true_shadow_katana";
+    public static final String INFLUX_WAVER = "influx_waver";
+    public static final String STAR_WRATH = "star_wrath";
 
-    public static final String TREASUREBOX = "treasurebox";
-    public static final String SAGES_MANA_RING = "sagesmanaring";
+    public static final String TREASURE_BOX = "treasure_box";
+    public static final String SAGES_MANA_RING = "sages_mana_ring";
     public static final String EXCALIBER = "excaliber";
     public static final String FAILNAUGHT = "failnaught";
-    public static final String COREGOD = "coregod";
-    public static final String ACHILLEDSHIELD = "achilledshield";
-    public static final String BUDDHISTRELICS = "buddhistrelics";
+    public static final String CORE_GOD = "core_god";
+    public static final String ACHILLED_SHIELD = "achilled_shield";
+    public static final String BUDDHIST_RELICS = "buddhist_relics";
     public static final String CAMERA = "camera";
 
     public static final String SPIRIT_FUEL = "spirit_fuel";
@@ -82,7 +82,7 @@ public class LibItemNames {
     public static final String UNIVERSAL_PETAL = "universal_petal";
     public static final String ELEMENT_RUNE = "element_rune";
     public static final String SIN_RUNE = "sin_rune";
-    public static final String CHALLENGETICKET = "challengeticket";
+    public static final String CHALLENGE_TICKET = "challenge_ticket";
 
     public static final String SMELT_LENS = "smelt_lens";
     public static final String MANA_LENS = "mana_lens";
@@ -91,13 +91,13 @@ public class LibItemNames {
     public static final String POTION_LENS = "potion_lens";
     public static final String SUPER_CONDUCTOR_LENS = "super_conductor_lens";
 
-    public static final String FOX_EAR = "foxear";
-    public static final String FOX_MASK = "foxmask";
-    public static final String THUG_LIFE = "thuglife";
-    public static final String SUPER_CROWN = "supercrown";
-    public static final String RED_SCARF = "redscarf";
+    public static final String FOX_EAR = "fox_ear";
+    public static final String FOX_MASK = "fox_mask";
+    public static final String THUG_LIFE = "thug_life";
+    public static final String SUPER_CROWN = "super_crown";
+    public static final String RED_SCARF = "red_scarf";
     public static final String PYLON = "pylon";
-    public static final String BLACK_GLASSES = "blackglasses";
+    public static final String BLACK_GLASSES = "black_glasses";
     public static final String MASK = "mask";
 
     public static final String ARMOR_MAID_HELM = "maid_helm";
@@ -130,10 +130,10 @@ public class LibItemNames {
     public static final String ARMOR_SILENTSAGES_LEGS = "silentsages_legs";
     public static final String ARMOR_SILENTSAGES_BOOTS = "silentsages_boots";
 
-    public static final String REWARDBAGA = "rewardbaga";
-    public static final String REWARDBAGB = "rewardbagb";
-    public static final String REWARDBAGC = "rewardbagc";
-    public static final String REWARDBAGD = "rewardbagd";
+    public static final String REWARD_BAG_A = "reward_bag_a";
+    public static final String REWARD_BAG_B = "reward_bag_b";
+    public static final String REWARD_BAG_C = "reward_bag_c";
+    public static final String REWARD_BAG_D = "reward_bag_d";
 
     public static final String MANASTEEL_HAMMER = "manasteel_hammer";
     public static final String ELEMENTIUM_HAMMER = "elementium_hammer";
