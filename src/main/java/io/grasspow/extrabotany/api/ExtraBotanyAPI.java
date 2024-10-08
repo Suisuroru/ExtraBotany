79f6c43d3427e0a148e01200739002d50db70b8e
grasspow <grasspow@outlook.com>
2024-10-08 15:33:51 +0800

    [feat]: add armor_material

--- src/main/java/io/grasspow/extrabotany/api/ExtraBotanyAPI.java
+++ src/main/java/io/grasspow/extrabotany/api/ExtraBotanyAPI.java
@@ -2,10 +2,13 @@ package io.grasspow.extrabotany.api;
 
 import io.grasspow.extrabotany.common.impl.ExtraBotanyApiImpl;
 import io.grasspow.extrabotany.common.libs.LibMisc;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import vazkii.botania.api.ServiceUtil;
+
 
 public interface ExtraBotanyAPI {
     String MOD_ID = LibMisc.MOD_ID;
@@ -15,6 +18,20 @@ public interface ExtraBotanyAPI {
     static ExtraBotanyAPI instance() {
         return INSTANCE;
     }
+
+    ExtraBotanyArmorMaterial getMaidArmorMaterial();
+
+    ExtraBotanyArmorMaterial getMikuArmorMaterial();
+
+    ExtraBotanyArmorMaterial getGoblinSlayerArmorMaterial();
+
+    ExtraBotanyArmorMaterial getShadowWarriorArmorMaterial();
+
+    ExtraBotanyArmorMaterial getShootingGuardianArmorMaterial();
+
+    ExtraBotanyArmorMaterial getSilentSagesArmorMaterial();
+
+    void addPotionEffect(LivingEntity entity, MobEffect potion, int max);
 
     default int apiVersion() {
         return 0;
